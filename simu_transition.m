990f5914296b302d684e41d314c005c9808f71f8
GeoffNN <geoffrey.negiar@polytechnique.edu>
2016-11-13 13:17:42 +0100

    Removed .m solutions

--- simu_transition.m
+++
@@ -1,7 +0,0 @@
-function [xnew,reward] = simu_transition(x,a,M,K,h,c,p,D)
-d=simu(D); % simulate the number of customers that arrive this week
-xnew=max(0,min((x+a),M)-d); % compute the new state
-reward= -K*(a>0) - c*max(0,min(x+a,M) - x) - h*x + p*min([d , x+a, M]); % compute the associated reward
-end
-
-
